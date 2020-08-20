3ce111a31fc7bf89efa3bbfd3183c17d09998b26
Tamas Gal <tgal@km3net.de>
2020-08-21 00:58:38 +0200

    Improve provenance structure

--- thepipe/provenance.py
+++ thepipe/provenance.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from datetime import datetime
+from importlib import import_module
 import json
 import os
 import platform
@@ -9,6 +10,9 @@ import sys
 import types
 import uuid
 
+from pip._internal.operations import freeze
+
+from .logger import get_logger
 from .tools import peak_memory_usage
 
 ENV_VARS_TO_LOG = [
@@ -26,26 +30,43 @@ ENV_VARS_TO_LOG = [
     "CONDA_SHLVL",
 ]
 
+ENV_VARS_IN_CI_TO_LOG = [
+    "APPVEYOR",
+    "CI",
+    "CIRCLECI",
+    "CONTINUOUS_INTEGRATION",
+    "GITHUB_ACTIONS",
+    "GITLAB_CI",
+    "TF_BUILD",
+    "TRAVIS",
+]
 
-def imported_modules():
-    """All imported modules"""
-    for name, val in globals().items():
-        if isinstance(val, types.ModuleType):
-            yield val
+log = get_logger("Provenance")
 
 
-def module_version(module):
-    try:
-        return module.__version__
-    except AttributeError:
-        try:
-            return module.version
-        except AttributeError:
-            return "unknown"
+def python_packages():
+    """All installed Python packages"""
+    packages = []
+    for entry in freeze.freeze(exclude_editable=True):
+        name, version = entry.split("==")
+        packages.append(dict(name=name, version=version))
+    return packages
 
 
 def _getenv():
-    return {var: os.getenv(var) for var in ENV_VARS_TO_LOG}
+    """Returns the environment variables while maskng sensitive data"""
+    env = {var: os.getenv(var) for var in ENV_VARS_TO_LOG}
+    for var in ENV_VARS_IN_CI_TO_LOG:
+        value = os.getenv(var, "").lower()
+        if value in ["", None]:
+            env[var] = None
+        elif os.getenv(var) in ["true", "t", "yes", "y", "1"]:
+            env[var] = "true"
+        elif os.getenv(var) in ["false", "f", "no", "n", "0"]:
+            env[var] = "false"
+        else:
+            env[var] = "other"
+    return env
 
 
 class Singleton(type):
@@ -65,11 +86,37 @@ class Provenance(metaclass=Singleton):
     """
 
     def __init__(self):
+        log.info("Initialising provenance tracking")
         self._activities = []
+        self._backlog = []
 
     def start_activity(self, name):
-        activity = Activity(name)
-        activity.start()
+        log.info("Starting activity '{}'".format(name))
+        self._activities.append(Activity(name))
+
+    def finish_activity(self):
+        try:
+            activity = self._activities.pop()
+        except IndexError:
+            log.error("There is no activity to finish.")
+            return
+        else:
+            log.info("Finishing activity '{}'".format(activity.name))
+            activity.finish()
+            self._backlog.append(activity)
+
+    @property
+    def provenance(self):
+        return [a.provenance for a in self._backlog]
+
+    def as_json(self, **kwargs):
+        """Dump provenance as JSON string. `kwargs` are passed to `json.dumps`"""
+        return json.dumps(self.provenance, **kwargs)
+
+    def reset(self):
+        log.info("Resetting provenance")
+        self._activities = []
+        self._backlog = []
 
 
 class Activity:
@@ -78,13 +125,20 @@ class Activity:
         self._data = dict(
             uuid=str(uuid.uuid4()),
             name=name,
-            start={},
+            start=system_state(),
             stop={},
-            system={},
+            system=system_provenance(),
             input=[],
             output=[],
             samples=[],
         )
+
+    def finish(self):
+        self._data["stop"] = system_state()
+
+    @property
+    def provenance(self):
+        return self._data
 
 
 def isotime(timestamp):
@@ -97,18 +151,20 @@ def now():
     return datetime.now(pytz.utc).isoformat()
 
 
-def _system_load():
-    """CPU and memory status"""
-    return dict(peak_memory=peak_memory_usage())
+def system_state():
+    return dict(time_utc=now(), peak_memory=peak_memory_usage())
 
 
-def _system_provenance():
+def system_provenance():
     """Provenance information of the system configuration"""
 
     bits, linkage = platform.architecture()
 
     return dict(
+        thepipe_version=import_module("thepipe").version,
         executable=sys.executable,
+        arguments=sys.argv,
+        environment=_getenv(),
         platform=dict(
             architecture_bits=bits,
             architecture_linkage=linkage,
@@ -127,8 +183,7 @@ def _system_provenance():
             version=platform.python_version_tuple(),
             compiler=platform.python_compiler(),
             implementation=platform.python_implementation(),
+            packages=python_packages(),
         ),
-        environment=_getenv(),
-        arguments=sys.argv,
         start_time_utc=now(),
     )
